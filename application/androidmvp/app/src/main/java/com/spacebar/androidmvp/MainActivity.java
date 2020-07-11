656e89dc1e97f81aadd2c32647984882e71291a1
Shanindu Rajapaksha <shanindu.rajapaksha91@gmail.com>
2020-07-11 17:49:15 +0530

    Initial commit

---
+++ application/androidmvp/app/src/main/java/com/spacebar/androidmvp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.spacebar.androidmvp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
